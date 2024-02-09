0198e7f5f4a90e4c1a013cf89d482d2f16d8659c
Anish Saha <anish27863@gmail.com>
2024-02-09 10:47:54 +0530

    complete

--- frequency.py
+++ frequency.py
@@ -1,2 +1,12 @@
 s=input("Enter a string")
 ns=""
+for i in s:
+    if i not in ns:
+        ns=ns+i
+#print(ns)
+for j in ns:
+    c=0
+    for k in s:
+        if(j==k):
+            c=c+1
+    print(f"frequency of {j} is {c}")               
